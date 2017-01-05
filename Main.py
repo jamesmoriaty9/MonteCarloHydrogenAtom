80be4d5591394d3cf046ddb028563fa74a43b560
till <tillvonule@yahoo.de>
2017-01-05 17:35:42 +0100

    Metropolis Algorithm
    The metropolis algorithm is finished and I tested if i can call a
    python script within the main script. Which by the way works well.

--- Main.py
+++ Main.py
@@ -2,5 +2,11 @@
 __author__ = 'Till'
 import math
 import scipy as sci
+import Metropolis_Algorithm
+
+
+
+#here you can see that the implementation of other pythonscripts within the main script works perfectly :)
+print Metropolis_Algorithm.MetropolisAlgorithm(5000,0.01), Metropolis_Algorithm.probability(5000,0.01)
 
 
--- Metropolis_Algorithm.py
+++ Metropolis_Algorithm.py
@@ -2,7 +2,8 @@
 __author__ = 'Till'
 
 import math
-import scipy
+import numpy
+
 
 
 """
@@ -13,4 +14,24 @@ else the state is excepted by a given Probability depending on the temperature
 
 
 def probability(temperature,energydifference):
-    return math.exp(-energydifference/(temperature))
+    """
+    :param temperature: the temperature at which the system is currently running in Kelvin
+    :param energydifference: the difference between the previous and new energy state in Hartrees (Atomic units)
+    :return: a probability with which the new value will be accepted even if the energy is higher than the previous one
+    """
+    k = 3.16681*10**(-6)
+    return math.exp(-energydifference/(temperature*k))
+
+def MetropolisAlgorithm(temperature=1,energydifference=0):
+    """
+    :param temperature:is the temperature at which the system is running in Kelvin
+    :param energydifference: the difference between the previous and new energy state in Hartrees (Atomic units)
+    :return:1 if the State should be passed and 0 if not
+    """
+    accepted = probability(temperature,energydifference)
+    if energydifference <= 0:
+        return 1
+    elif numpy.random.choice(numpy.arange(1, 3), p=[ accepted,1-accepted])==1:
+        return 1
+    else:
+        return 0
