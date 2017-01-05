49dae0ae62d87610e8e92606e5c7d5342c85dc8e
till <tillvonule@yahoo.de>
2017-01-05 21:27:35 +0100

    schönheitskorrekturen

--- Main.py
+++ Main.py
@@ -5,8 +5,5 @@ import scipy as sci
 import Metropolis_Algorithm
 
 
-
-#here you can see that the implementation of other pythonscripts within the main script works perfectly :)
-print Metropolis_Algorithm.MetropolisAlgorithm(5000,0.01), Metropolis_Algorithm.probability(5000,0.01)
-
-
+"""here you can see that the implementation of other pythonscripts within the main script works perfectly :)"""
+print Metropolis_Algorithm.metropolisalgorithm(5000, 0.01), Metropolis_Algorithm.probability(5000, 0.01)
--- Metropolis_Algorithm.py
+++ Metropolis_Algorithm.py
@@ -4,34 +4,33 @@ __author__ = 'Till'
 import math
 import numpy
 
-
-
 """
-The task is to choose whether a new state is chosen or not depending on the new Energystate.
-If the Energystate is lower than the previous the new state is automatically excepted
-else the state is excepted by a given Probability depending on the temperature
+The task is to choose whether a new state is chosen or not depending on the new energystate.
+If the energystate is lower than the previous the new state is automatically excepted
+else the state is excepted by a given probability depending on the temperature
 """
 
 
-def probability(temperature,energydifference):
+def probability(temperature, energydifference):
     """
-    :param temperature: the temperature at which the system is currently running in Kelvin
-    :param energydifference: the difference between the previous and new energy state in Hartrees (Atomic units)
+    :param temperature: the temperature at which the system is currently running. (in Kelvin)
+    :param energydifference: the difference between the previous and new energy state. (in Hartrees (Atomic units))
     :return: a probability with which the new value will be accepted even if the energy is higher than the previous one
     """
     k = 3.16681*10**(-6)
     return math.exp(-energydifference/(temperature*k))
 
-def MetropolisAlgorithm(temperature=1,energydifference=0):
+
+def metropolisalgorithm(temperature=1, energydifference=0):
     """
-    :param temperature:is the temperature at which the system is running in Kelvin
+    :param temperature: the temperature at which the system is running in Kelvin
     :param energydifference: the difference between the previous and new energy state in Hartrees (Atomic units)
     :return:1 if the State should be passed and 0 if not
     """
-    accepted = probability(temperature,energydifference)
+    accepted = probability(temperature, energydifference)
     if energydifference <= 0:
         return 1
-    elif numpy.random.choice(numpy.arange(1, 3), p=[ accepted,1-accepted])==1:
+    elif numpy.random.choice(numpy.arange(1, 3), p=[accepted, 1-accepted]) == 1:
         return 1
     else:
         return 0
