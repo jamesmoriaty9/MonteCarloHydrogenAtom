7fc537f7a10a1716bfb8ffc33fc471a65cd43caa
till <tillvonule@yahoo.de>
2017-01-10 10:41:17 +0100

    Schönheitsfehler

--- Main.py
+++ Main.py
@@ -1,9 +1,33 @@
 #!/usr/bin/env python
 __author__ = 'Till'
 import math
-import scipy as sci
 import Metropolis_Algorithm
+import numpy
 
 
 """here you can see that the implementation of other pythonscripts within the main script works perfectly :)"""
-print Metropolis_Algorithm.metropolisalgorithm(5000, 0.01), Metropolis_Algorithm.probability(5000, 0.01)
+#print Metropolis_Algorithm.metropolisalgorithm(1000000, 1), Metropolis_Algorithm.probability(1000000, 1)
+
+def energystate(radius):
+    """
+    :param radius: the radius where the energystate will be calculated
+    :return: the energystate of the atom
+    """
+    return -0.5*math.log(radius)
+
+
+def montecarlo(temperature, steps):
+    """
+    :param temperature: the temperature where the program will run the montecarlo algorithm
+    :param steps: how many steps it will take
+    :return: the groundstate energy of the hydrogen atom and the groundstate radius
+    """
+    randomwalker = numpy.random.randn(steps)
+    radius = 2
+    for delr in randomwalker:
+        energydiff = energystate(radius + delr) - energystate(radius)
+        if Metropolis_Algorithm.metropolisalgorithm(temperature, energydiff) == 1:
+            radius += delr
+    return "The groundstate energy is ", energystate(radius), " at the radius of ", radius
+
+print montecarlo(100, 10)
--- Metropolis_Algorithm.py
+++ Metropolis_Algorithm.py
@@ -18,7 +18,8 @@ def probability(temperature, energydifference):
     :return: a probability with which the new value will be accepted even if the energy is higher than the previous one
     """
     k = 3.16681*10**(-6)
-    return math.exp(-energydifference/(temperature*k))
+    print numpy.exp(-energydifference/(temperature*k)), energydifference
+    return numpy.exp(-energydifference/(temperature*k))
 
 
 def metropolisalgorithm(temperature=1, energydifference=0):
@@ -27,10 +28,11 @@ def metropolisalgorithm(temperature=1, energydifference=0):
     :param energydifference: the difference between the previous and new energy state in Hartrees (Atomic units)
     :return:1 if the State should be passed and 0 if not
     """
-    accepted = probability(temperature, energydifference)
+    # accepted = probability(temperature, energydifference)
+    # you can not define the prob. first, because the exponential function might give an overflow at 0 energydifference
     if energydifference <= 0:
         return 1
-    elif numpy.random.choice(numpy.arange(1, 3), p=[accepted, 1-accepted]) == 1:
+    elif numpy.random.choice(numpy.arange(1, 3), p=[probability(temperature, energydifference), 1-probability(temperature, energydifference)]) == 1:
         return 1
     else:
         return 0
